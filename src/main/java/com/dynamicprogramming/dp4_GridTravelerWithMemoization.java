57733e6c04a0f62475f38d16e834cbca3ae4eb45
YKRoshan <yashawant.nita@boeing.com>
2021-06-18 11:51:07 +0530

    DP code

--- src/main/java/com/dynamicprogramming/dp4_GridTravelerWithMemoization.java
+++ src/main/java/com/dynamicprogramming/dp4_GridTravelerWithMemoization.java
@@ -5,15 +5,14 @@ import java.util.Map;
 
 public class dp4_GridTravelerWithMemoization {
     public static int gridTraveler(int n, int m, Map<String, Integer> memo) {
-        String key = (Integer.toString(n) + Integer.toString(m));
+        String key = (Integer.toString(n) + "," + Integer.toString(m));
         if (memo.get(key) != null)
             return memo.get(key);
         if (n == 0 || m == 0)
             return 0;
         if (n == 1 && m == 1)
             return 1;
-        memo.put(key, gridTraveler(n - 1, m, memo) +
-            gridTraveler(n, m - 1, memo));
+        memo.put(key, gridTraveler(n - 1, m, memo) + gridTraveler(n, m - 1, memo));
         return memo.get(key);
     }
 
